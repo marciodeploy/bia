a925caeee5def5f298db694ade2f8b72fb9e36e2
EC2 Default User <ec2-user@ip-10-1-3-244.ec2.internal>
2024-10-31 22:49:59 +0000

    31 10 2024 1

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
+erro() {
+  echo "erro: $1"
+  exit 1
+}
+
+checar_ultimo_comando() {
+  if [ $? -ne 0 ]; then
+    erro "erro no deploy -- parando tudo"
+  fi
+}
+
 versao=$(git rev-parse HEAD | cut -c 1-7)
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 864981753284.dkr.ecr.us-east-1.amazonaws.com
+checar_ultimo_comando
 docker build -t meddiflux .
 docker tag meddiflux:latest 864981753284.dkr.ecr.us-east-1.amazonaws.com/meddiflux:$versao
 docker push 864981753284.dkr.ecr.us-east-1.amazonaws.com/meddiflux:$versao
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,7 +20,7 @@ const Header = ({ title, onAdd, showAdd }) => {
 };
 
 Header.defaultProps = {
-	title: "Meddiflux - Script V241030-2",
+	title: "Meddiflux - Script V241031-1",
 };
 
 export default Header;
